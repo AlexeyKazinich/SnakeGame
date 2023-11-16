e2c40fe2bd8624b93e5a2e6042abb24229221c81
AlexeyKazinich <alexey.kazinich@gmail.com>
2023-11-15 22:55:33 -0500

    refactored code a bit removing alot of empty space

--- Utilities/tilemap.py
+++ Utilities/tilemap.py
@@ -2,84 +2,90 @@ import pygame
 from Utilities.objects import Rectangle, Text
 from random import randint
 
-NEIGHBOR_OFFSETS = [(-1,0), (-1,-1), (0,-1), (1,-1), (0,0), (1,0), (-1,1),(0,1),(1,1)]
+NEIGHBOR_OFFSETS = [(-1, 0), (-1, -1), (0, -1), (1, -1), (0, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
+
 
 class Tile:
-    def __init__(self,screen: pygame.display, pos: tuple,tile_size: int = 16,fill_type: str = "outline",color: pygame.Color = pygame.Color(255,255,255,255)):
+    def __init__(self, screen: pygame.display, pos: tuple, tile_size: int = 16, fill_type: str = "outline",
+                 color: pygame.Color = pygame.Color(255, 255, 255, 255)):
         self.tile_size = tile_size
         self.pos = pos
-        self.previous_pos = (0,0)
+        self.previous_pos = (0, 0)
         self.screen = screen
         self.fill_type = fill_type
         self.color = color
-        self.rect = Rectangle(0,0,0,0,self.screen,(255,255,255))
-        
-        
-    
-    def draw(self,offset:tuple = (0,0),color: pygame.Color = pygame.Color(0,0,0,255)) -> None:
+        self.rect = Rectangle(0, 0, 0, 0, self.screen, (255, 255, 255))
+
+    def draw(self, offset: tuple = (0, 0), color: pygame.Color = pygame.Color(0, 0, 0, 255)) -> None:
         """draws the rectangle"""
         if self.fill_type == "fill":
-            self.rect = Rectangle((int(self.pos[0]) + int(offset[0]))*self.tile_size,(int(self.pos[1])+int(offset[1]))*self.tile_size,self.tile_size,self.tile_size,self.screen,self.color)
+            self.rect = Rectangle((int(self.pos[0]) + int(offset[0])) * self.tile_size,
+                                  (int(self.pos[1]) + int(offset[1])) * self.tile_size, self.tile_size, self.tile_size,
+                                  self.screen, self.color)
             self.rect.draw()
         elif self.fill_type == "outline":
-            self.rect = Rectangle((self.pos[0] + int(offset[0])) *self.tile_size,(self.pos[1]+offset[1])*self.tile_size,self.tile_size,self.tile_size,self.screen,color)
+            self.rect = Rectangle((self.pos[0] + int(offset[0])) * self.tile_size,
+                                  (self.pos[1] + offset[1]) * self.tile_size, self.tile_size, self.tile_size,
+                                  self.screen, color)
             self.rect.draw_box()
-        
-            
-    def collide(self,rect: Rectangle) -> bool:
+
+    def collide(self, rect: Rectangle) -> bool:
         """checks if the rectangle is colliding with a different rectangle"""
-        return self.rect.collidepoint((rect.x,rect.y))
-        
-        
+        return self.rect.collidepoint((rect.x, rect.y))
+
+
 class Tilemap:
-    def __init__(self,screen: pygame.display, tile_size: int = 16):
-        self.head_color = pygame.Color(0,200,0,255)
-        self.body_color = pygame.Color(0,255,0,255)
-        self.apple_color = pygame.Color(255,0,0,255)
-        
+    def __init__(self, screen: pygame.display, tile_size: int = 16):
+        self.head_color = pygame.Color(0, 200, 0, 255)
+        self.body_color = pygame.Color(0, 255, 0, 255)
+        self.apple_color = pygame.Color(255, 0, 0, 255)
+
         self.tile_size = tile_size
-        self.tilemap ={}
+        self.tilemap = {}
         self.offgrid_tiles = {}
-        self.snake = {"head": Tile(screen, (randint(0,10),randint(0,10)),tile_size,"fill",self.head_color)}
+        self.snake = {"head": Tile(screen, (randint(0, 10), randint(0, 10)), tile_size, "fill", self.head_color)}
         self.snake_length = 1
         self.screen = screen
-        self.grid_size = self.get_grid_size() #24,18
-        
-        self.apple = Tile(self.screen,(randint(0,10),randint(0,10)),32,"fill",self.apple_color)
-    
-    def draw_grid(self,size:tuple) -> None:
+        self.grid_size = self.get_grid_size()  # 24,18
+
+        self.apple = Tile(self.screen, (randint(0, 10), randint(0, 10)), 32, "fill", self.apple_color)
+
+    def draw_grid(self, size: tuple) -> None:
         """call this to draw a grid"""
         for i in range(size[0]):
             for j in range(size[0]):
-                get_loc = (i,j)
-                self.tilemap[f"{get_loc[0]},{get_loc[1]}"] = Tile(self.screen,get_loc,self.tile_size)
-    
-    def get_loc_part(self,name: str) -> tuple:
+                get_loc = (i, j)
+                self.tilemap[f"{get_loc[0]},{get_loc[1]}"] = Tile(self.screen, get_loc, self.tile_size)
+
+    def get_loc_part(self, name: str) -> tuple:
         """gets the location from the dictionary for a given snake part"""
-        location = (self.snake[name].pos[0],self.snake[name].pos[1] + 1)
+        location = (self.snake[name].pos[0], self.snake[name].pos[1] + 1)
         return location
-    
+
     def extend_snake(self) -> None:
         """extends the snake by 1"""
         if self.snake_length == 1:
-            self.snake[f"body{self.snake_length}"] =Tile(self.screen,self.get_loc_part("head"),fill_type="fill",color=self.body_color,tile_size=self.tile_size)
+            self.snake[f"body{self.snake_length}"] = Tile(self.screen, self.get_loc_part("head"), fill_type="fill",
+                                                          color=self.body_color, tile_size=self.tile_size)
             self.snake_length += 1
         else:
-            self.snake[f"body{self.snake_length}"] = Tile(self.screen,self.get_loc_part(f"body{self.snake_length-1}"),fill_type="fill",color=self.body_color,tile_size=self.tile_size)
+            self.snake[f"body{self.snake_length}"] = Tile(self.screen,
+                                                          self.get_loc_part(f"body{self.snake_length - 1}"),
+                                                          fill_type="fill", color=self.body_color,
+                                                          tile_size=self.tile_size)
             self.snake_length += 1
-    
-    def get_grid_size(self) -> tuple:
-        size = [self.screen.get_width(),self.screen.get_height()]
-        return (int(size[0] / self.tile_size),int(size[1] / self.tile_size)) #returns 25,19
 
-    
+    def get_grid_size(self) -> tuple:
+        size = [self.screen.get_width(), self.screen.get_height()]
+        return (int(size[0] / self.tile_size), int(size[1] / self.tile_size))  # returns 25,19
+
     def fill_screen(self) -> None:
         """fills the screen with a grid"""
         for i in range(self.grid_size[0]):
             for j in range(self.grid_size[1]):
-                get_loc = (i,j)
-                self.tilemap[f"{get_loc[0]},{get_loc[1]}"] = Tile(self.screen,get_loc,self.tile_size)
-    
+                get_loc = (i, j)
+                self.tilemap[f"{get_loc[0]},{get_loc[1]}"] = Tile(self.screen, get_loc, self.tile_size)
+
     def get_tiles_around(self) -> list:
         """gets the tiles around the head to check for collisions"""
         tiles = []
@@ -92,48 +98,48 @@ class Tilemap:
             except KeyError:
                 print("out of bounds")
         return tiles
-    
-    def draw(self,offset:tuple = (0,0),color: pygame.Color = pygame.Color(255,255,255,255)):
+
+    def draw(self, offset: tuple = (0, 0), color: pygame.Color = pygame.Color(255, 255, 255, 255)):
         self.fill_screen()
         for _, value in self.tilemap.items():
             value.draw()
         for _, value in self.snake.items():
             value.draw()
         self.apple.draw()
-            
+
     def update_apple(self) -> None:
         """relocates the apple to a new location"""
-        self.apple = Tile(self.screen,(randint(0,self.grid_size[0]-1),randint(0,self.grid_size[1]-1)),self.tile_size,"fill",self.apple_color)
-    
-    
-    def logic(self,move:str) -> None:
+        self.apple = Tile(self.screen, (randint(0, self.grid_size[0] - 1), randint(0, self.grid_size[1] - 1)),
+                          self.tile_size, "fill", self.apple_color)
+
+    def logic(self, move: str) -> None:
         """runs the logic for the tilemap"""
-        #check collision with apple
+        # check collision with apple
         if self.snake["head"].collide(self.apple.rect):
             self.extend_snake()
             self.update_apple()
-        
-        #check collision with self
+
+        # check collision with self
         tiles = self.get_tiles_around()
         for tile in tiles:
             if self.snake["head"].collide(tile.rect):
                 print("BOOM")
-        
+
         count = 1
         for key, value in self.snake.items():
             if key == "head" and move == "right":
                 value.previous_pos = value.pos
-                value.pos = (value.pos[0] + 1,value.pos[1])
+                value.pos = (value.pos[0] + 1, value.pos[1])
             elif key == "head" and move == "left":
                 value.previous_pos = value.pos
-                value.pos = (value.pos[0] - 1,value.pos[1])
+                value.pos = (value.pos[0] - 1, value.pos[1])
             elif key == "head" and move == "up":
                 value.previous_pos = value.pos
-                value.pos = (value.pos[0],value.pos[1]-1)
+                value.pos = (value.pos[0], value.pos[1] - 1)
             elif key == "head" and move == "down":
                 value.previous_pos = value.pos
-                value.pos = (value.pos[0],value.pos[1]+1)
-                
+                value.pos = (value.pos[0], value.pos[1] + 1)
+
             elif key == "body1":
                 value.previous_pos = value.pos
                 value.pos = self.snake["head"].previous_pos
@@ -141,15 +147,14 @@ class Tilemap:
                 value.previous_pos = value.pos
                 value.pos = self.snake[f"body{count}"].previous_pos
                 count += 1
-        #left->right
-        if self.snake["head"].pos[0] > self.grid_size[0] -1:
-            self.snake["head"].pos = (0,self.snake["head"].pos[1])
+        # left->right
+        if self.snake["head"].pos[0] > self.grid_size[0] - 1:
+            self.snake["head"].pos = (0, self.snake["head"].pos[1])
         elif self.snake["head"].pos[0] < 0:
-            self.snake["head"].pos = (self.grid_size[0] -1,self.snake["head"].pos[1])
-            
-        #up->down
+            self.snake["head"].pos = (self.grid_size[0] - 1, self.snake["head"].pos[1])
+
+        # up->down
         if self.snake["head"].pos[1] < 0:
-            self.snake["head"].pos = (self.snake["head"].pos[0],self.grid_size[1]-1)
-        elif self.snake["head"].pos[1] > self.grid_size[1] -1:
-            self.snake["head"].pos = (self.snake["head"].pos[0],0)
-        
+            self.snake["head"].pos = (self.snake["head"].pos[0], self.grid_size[1] - 1)
+        elif self.snake["head"].pos[1] > self.grid_size[1] - 1:
+            self.snake["head"].pos = (self.snake["head"].pos[0], 0)
